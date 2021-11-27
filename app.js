2929e08b3b7bdd21db5006be00422cb57c34d004
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-27 15:54:25 +0900

    Updated canvas
    painting line

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 const stopPainting = () => {
   painting = false;
 };
+const startPainting = () => {
+  painting = true;
+};
 
 const handleCanvasMove = (event) => {
   const { offsetX: x, offsetY: y } = event;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 const handleMouseDown = (event) => {
   painting = true;
 };
 
-const handleMouseUp = (event) => {
-  stopPainting();
-};
-
 if (canvas) {
   canvas.addEventListener("mousemove", handleCanvasMove);
   canvas.addEventListener("mousedown", handleMouseDown);
-  canvas.addEventListener("mouseup", handleMouseUp);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
