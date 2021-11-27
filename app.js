0c402aa425862d7c1800d52ecff356e4498d7de8
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-27 20:20:52 +0900

    Updated save button

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 canvas.width = 500;
 canvas.height = 500;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 500, 500); //default background-color
+
 ctx.strokeStyle = "black";
+ctx.fillStyle = "black";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 const stopPainting = () => {
   painting = false;
 };
-const startPainting = () => {
-  painting = true;
+const startPainting = (event) => {
+  if (!filling && event.which === 1) {
+    painting = true;
+  }
 };
 
 const handleCanvasMove = (event) => {
@@ -29,15 +38,19 @@ const handleCanvasMove = (event) => {
   }
 };
 
-const handleMouseDown = (event) => {
-  painting = true;
+const clickCanvas = () => {
+  if (filling) {
+    ctx.fillRect(0, 0, 500, 500);
+  }
 };
 
 if (canvas) {
   canvas.addEventListener("mousemove", handleCanvasMove);
-  canvas.addEventListener("mousedown", handleMouseDown);
+  canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", clickCanvas);
+  canvas.addEventListener("contextmenu", (event) => event.preventDefault());
 }
 
 const handleColor = (event) => {
@@ -47,6 +60,7 @@ const handleColor = (event) => {
     },
   } = event;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 };
 
 if (colors) {
@@ -63,4 +77,30 @@ const handleRange = (event) => {
 
 if (range) {
   range.addEventListener("input", handleRange);
+}
+
+const handleMode = () => {
+  if (filling) {
+    filling = false;
+    mode.innerText = "Fill";
+    range.disabled = false;
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+    range.disabled = true;
+  }
+};
+
+if (mode) {
+  mode.addEventListener("click", handleMode);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", () => {
+    const canvasUrl = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = canvasUrl;
+    link.download = "canvas-image";
+    link.click();
+  });
 }
